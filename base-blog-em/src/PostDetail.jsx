941594214dacd1e64b75c344354b740f9dbdca7a
Razvan Anisia <anisia.razvan@gmail.com>
2025-12-16 15:18:00 +0200

    fetching posts, comments, pagination

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,21 +1,29 @@
-import { fetchComments } from "./api";
-import "./PostDetail.css";
+import { fetchComments } from './api';
+import { useQuery } from '@tanstack/react-query';
+
+import './PostDetail.css';
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading } = useQuery({
+    queryKey: ['comments', post.id],
+    queryFn: () => fetchComments(post.id),
+    staleTime: 2000,
+    gcTime: 5000,
+  });
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
+      {isLoading
+        ? 'Loading comments'
+        : data.map((comment) => (
+            <li key={comment.id}>
+              {comment.email}: {comment.body}
+            </li>
+          ))}
     </>
   );
 }
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,35 +1,65 @@
-import { useState } from "react";
-
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
+import { useState, useEffect } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { fetchPosts, deletePost, updatePost } from './api';
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+  const queryClient = useQueryClient();
 
-  // replace with useQuery
-  const data = [];
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery({
+        queryKey: ['posts', nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }, [currentPage, queryClient]);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['posts', currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    staleTime: 5000,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return 'error, something went wrong';
+  }
+
+  console.log(currentPage);
 
   return (
     <>
       <ul>
         {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
+          <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage === 1}
+          onClick={() => {
+            setCurrentPage((page) => {
+              return page - 1;
+            });
+          }}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((page) => page + 1);
+          }}>
           Next page
         </button>
       </div>
